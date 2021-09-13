93fd9db8a7e40847f2d4b8445b31859a1d616c64
Mohammad Amin Shahidi <amin7799sh@gmail.com>
2021-09-13 23:53:27 +0430

    Create "tokenize" function

--- Source-code.py
+++ Source-code.py
@@ -1,1 +1,12 @@
-#initial comment
+import javalang
+
+def tokenize(sourceCodeDirectory):
+    with open(sourceCodeDirectory) as sourceCode:
+        tokensList = []
+        for line in sourceCode:
+            tokens = list(javalang.tokenizer.tokenize(line))
+            for token in tokens:
+                tokensList.append(token)
+    return tokensList
+
+test = tokenize("Test-class.java")
