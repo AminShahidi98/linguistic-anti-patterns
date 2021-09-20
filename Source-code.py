f18ebb0e44e47ec6d91ddacbfc293dc3bc2ee7fe
Mohammad Amin Shahidi <amin7799sh@gmail.com>
2021-09-20 17:23:04 +0430

    Add "parse" method.

--- Source-code.py
+++ Source-code.py
@@ -1,10 +1,16 @@
 import javalang
 
 def tokenize(sourceCodeDirectory):
-    sourceCode = open('Test-class.java', 'r').read()
+    sourceCode = open(sourceCodeDirectory, 'r').read()
     tokensList = list(javalang.tokenizer.tokenize(sourceCode))
     return tokensList
 
-test = tokenize("Test-class.java")
-for t in test:
-    print(t)
+def parse(sourceCodeDirectory):
+    sourceCode = open(sourceCodeDirectory, 'r').read()
+    tree = javalang.parse.parse(sourceCode)
+    return tree
+
+tree = parse("Demo.java")
+for path, node in tree.filter(javalang.tree.MethodDeclaration):
+    print(path)
+    print(node.position)
