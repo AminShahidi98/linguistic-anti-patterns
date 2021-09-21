1d12b11374b63315cade8ab269fb273c6e50c5b0
Mohammad Amin Shahidi <amin7799sh@gmail.com>
2021-09-22 01:10:43 +0330

    Add GetMethodLAP class.

---
+++ Models.py
@@ -0,0 +1,7 @@
+class GetMethodLAP():
+    def __init__(self, node, LAPType, describtion):
+        self.node = node
+        self.LAPType = LAPType
+        self.describtion = describtion
+    def __str__(self):
+        return "GetMethodLAP(type = " + self.LAPType + ", name = " + self.node.name + ", line = " + str(self.node.position.line) + ")"
