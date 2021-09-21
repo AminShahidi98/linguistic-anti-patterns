2c99a52f233de55e014996908f7323055e04c986
Mohammad Amin Shahidi <amin7799sh@gmail.com>
2021-09-22 01:12:28 +0330

    Rename some files and add a To-Do line.

--- Demo.java
+++ Demo.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
+//test-test-test-test
 public class Demo {
 
-    static void myMethod() {
+    static String myMethod() {
         System.out.println("I just got executed!");
+        return("wew");
     }
 
     public static void main(String[] args) {
         myMethod();
+        
     }
 }
