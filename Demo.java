243a5b9d14c1914dbb04bbe906c3779e246ad89d
amin1998sh <amin7799sh@gmail.com>
2022-01-31 12:53:53 +0330

    Test type 2 LAPs.

--- Demo.java
+++ Demo.java
@@ -6,7 +6,7 @@ public class Demo {
     int getMyFood = 3;
     
     public static void main(String[] args) {
-        System.out.print(isValid(-11)); 
+        System.out.print(isValid(78687)); 
     }
 
     private static int[] yesisMethode(int[] x, String m) {
@@ -14,68 +14,50 @@ public class Demo {
         return x;
     }
 
+    public static void settestpy(String star) {
+    }
 
-
-    public static void calculateForgetMyFood(String star) {
+    public static void getMyFood(String star) {
         System.out.print(star);
     }
     
     int isFalse = 1;
 
-
+    public static void getbaaasss(String star) {
+    }
 
     public static int getisDobed(int d, String star) {
-    return complement_getisDobed(d, star);
+        isDobed *= d;
+        return isDobed;
     }
 
     public static int getyourDobed(int d, String star) {
-    return complement_getyourDobed(d, star);
-    }
-
-    public static boolean isValid(int d) {
-    double[] temp = complement_isValid(d);
-    int size = temp.length;
-    if (size == 0) {return false;}
-    else if (size == 1){
-        double tempDouble = temp[0];
-        if (tempDouble == 0) {return false;}
-        else {return true;}
-    }
-    else {return true;}
-    }
-
-
-    public static int complement_getisDobed(int d, String star) {
         isDobed *= d;
         return isDobed;
     }
 
-    public static int complement_getyourDobed(int d, String star) {
-        isDobed *= d;
-        return isDobed;
-    }
-
-    public static double[] complement_isValid(int d) {
+    public static byte[] isValid(int d) {
         if (d == 5) {
-            return new double[] {23};
+            return new byte[] {23};
         }
         else if (d == 4) {
-            double[] testS = {-23, 42};
+            byte[] testS = {-23, 42};
             return testS;
         }
         else if (d == 9) {
-            return new double[] {66};
+            return new byte[] {66};
         }
         else if (d == -11) {
-            double[] testS2 = {0.0d};
+            byte[] testS2 = {0};
             return testS2;
         }
         else if (d == -2) {
-            double[] testS2 = {};
+            byte[] testS2 = {};
             return testS2;
         }
         else {
-            return new double[] {1};
+            return new byte[] {1};
         }
     }
+
 }
